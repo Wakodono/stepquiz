de0ee6d14c56beef237d5f02c2422221b282013a
Wakodono <53127265+Wakodono@users.noreply.github.com>
2022-04-25 22:20:42 +0100

    updates and bug fixes

--- src/App.js
+++ src/App.js
@@ -1,66 +1,71 @@
-import React from 'react'
-import { useState } from 'react'
-import steps from './StepOutput.json'
+import React from "react";
+import { useState } from "react";
+import Container from "react-bootstrap/Container";
+import steps from "./StepOutput.json";
 
 const App = () => {
-  const scoreTotal = steps.data.getStep.stepQuiz.answerOptions.length
-//   const questions = steps.data.getStep.stepQuiz.answerOptions.map((question) => {
-// 	  return (question.answerText)
-//   })
-const questions = steps.data.getStep.stepQuiz
+//   const stepQuiz = steps.data.getStep.stepQuiz;
+//   Object.entries(stepQuiz)
 
-  console.log(questions)
+//   console.log(stepQuiz);
 
+  const questions = steps.data.getStep.stepQuiz;
 
   const [currentQuestion, setCurrentQuestion] = useState(0);
-	const [showScore, setShowScore] = useState(false);
-	const [score, setScore] = useState(0);
+  const [showScore, setShowScore] = useState(false);
+  const [score, setScore] = useState(0);
 
   const handleAnswerOptionClick = (isCorrect) => {
-		if (isCorrect) {
-			setScore(score + 1);
-		}
+    if (isCorrect) {
+      setScore(score + 1);
+    }
 
-		const nextQuestion = currentQuestion + 1;
-		if (nextQuestion < questions.length) {
-			setCurrentQuestion(nextQuestion);
-		} else {
-			setShowScore(true);
-		}
-
-	};
+    // const nextQuestion = currentQuestion + 1;
+    // if (nextQuestion < questions.length) {
+    // 	setCurrentQuestion(nextQuestion);
+    // } else {
+    // 	setShowScore(true);
+    // }
+  };
 
   const handleRestart = () => {
-    setCurrentQuestion(0)
-    setShowScore(false)
-    setScore(0)
-  }
+    setCurrentQuestion(0);
+    setShowScore(false);
+    setScore(0);
+  };
 
   return (
-		<div className='app'>
-			{/* HINT: replace "false" with logic to display the 
-      score when the user has answered all the questions */}
-			{false ? (
-				<div className='score-section'>You scored 1 out of {scoreTotal}</div>
-			) : (
-				<>
-					<div className='question-section'>
-						{/* <div className='question-count'>
-							<span>Question 1</span>/{questions.length + 1}
-						</div> */}
-						<div className='question-text'><h3>{questions.questionText}</h3></div>
-					</div>
-					<div className='answer-section'>
-						{
-							questions.answerOptions.map((answerOption, index) => (
-								<button>{answerOption.answerText}</button>
-							))
-						}
-					</div>
-				</>
-			)}
-		</div>
-	);
-}
+    <Container fluid>
+    	<div className="app">
+	      {showScore ? (
+	        <div className="score-section">
+	          You scored {score} out of {questions.length}
+	        </div>
+	      ) : (
+	        <>
+	          <div className="question-section">
+	            {/* <div className='question-count'>
+								<span>Question 1</span>/{questions.length + 1}
+							</div> */}
+	            <div className="question-text">
+	              <h3>{questions.questionText}</h3>
+	            </div>
+	            <div className="question-text">
+	              <p>Choose all the correct options you see</p>
+	            </div>
+	          </div>
+	          <div className="answer-section">
+	            {questions.answerOptions.map((answerOption, index) => (
+	              <button onClick={() => handleAnswerOptionClick(answerOption.isCorrect)} key={index}>
+	                {answerOption.answerText}
+	              </button>
+	            ))}
+	          </div>
+	        </>
+	      )}
+	    </div>
+    </Container>
+  );
+};
 
-export default App
+export default App;
