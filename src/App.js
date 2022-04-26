3deb95f2c93328788d8828d00b13a17ca5474786
Wakodono <53127265+Wakodono@users.noreply.github.com>
2022-04-26 09:45:00 +0100

    Responsiveness improvements

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { useState } from "react";
 import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 import steps from "./StepOutput.json";
 
 const App = () => {
@@ -35,35 +37,39 @@ const App = () => {
   };
 
   return (
-    <Container fluid>
-    	<div className="app">
-	      {showScore ? (
-	        <div className="score-section">
-	          You scored {score} out of {questions.length}
-	        </div>
-	      ) : (
-	        <>
-	          <div className="question-section">
-	            {/* <div className='question-count'>
-								<span>Question 1</span>/{questions.length + 1}
-							</div> */}
-	            <div className="question-text">
-	              <h3>{questions.questionText}</h3>
-	            </div>
-	            <div className="question-text">
-	              <p>Choose all the correct options you see</p>
-	            </div>
-	          </div>
-	          <div className="answer-section">
-	            {questions.answerOptions.map((answerOption, index) => (
-	              <button onClick={() => handleAnswerOptionClick(answerOption.isCorrect)} key={index}>
-	                {answerOption.answerText}
-	              </button>
-	            ))}
-	          </div>
-	        </>
-	      )}
-	    </div>
+    <Container>
+    	<Row>
+    		<Col xs={6}>
+    			<div className="app">
+			      {showScore ? (
+			        <div className="score-section">
+			          You scored {score} out of {questions.length}
+			        </div>
+			      ) : (
+			        <>
+			          <div className="question-section">
+			            {/* <div className='question-count'>
+										<span>Question 1</span>/{questions.length + 1}
+									</div> */}
+			            <div className="question-text">
+			              <h3>{questions.questionText}</h3>
+			            </div>
+			            <div className="question-text">
+			              <p>Choose all the correct options</p>
+			            </div>
+			          </div>
+			          <div className="answer-section">
+			            {questions.answerOptions.map((answerOption, index) => (
+			              <button onClick={() => handleAnswerOptionClick(answerOption.isCorrect)} key={index}>
+			                {answerOption.answerText}
+			              </button>
+			            ))}
+			          </div>
+			        </>
+			      )}
+			    </div>
+    		</Col>
+    	</Row>
     </Container>
   );
 };
